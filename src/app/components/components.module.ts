f3bb4e7e3bb213d93e887c3c6c28c648515dfbff
Timothy Oliver <oliverwakogi@live.com>
2019-09-23 01:49:45 +0300

    Refactor components

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,13 +5,14 @@ import { QuotesComponent } from './quotes/quotes.component';
 import { IonicModule } from '@ionic/angular';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { FormComponent } from './form/form.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @NgModule({
-  declarations: [QuotesComponent, QuoteDetailsComponent, FormComponent],
+  declarations: [QuotesComponent, QuoteDetailsComponent, FormComponent, QuoteComponent],
   imports: [
     CommonModule,
     IonicModule
   ],
-  exports: [QuotesComponent, QuoteDetailsComponent, FormComponent]
+  exports: [QuotesComponent, QuoteDetailsComponent, FormComponent, QuoteComponent]
 })
 export class ComponentsModule { }
